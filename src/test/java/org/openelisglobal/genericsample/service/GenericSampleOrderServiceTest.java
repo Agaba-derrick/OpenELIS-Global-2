ff76c4a7cf5e2f81a83d5b92b6f08ce661002845
Agaba-derrick <agabaderrick18@gmail.com>
2026-08-03 10:23:39 +0300

    fix(test): use exact resyncSequence arguments requested by reviewer

--- src/test/java/org/openelisglobal/genericsample/service/GenericSampleOrderServiceTest.java
+++ src/test/java/org/openelisglobal/genericsample/service/GenericSampleOrderServiceTest.java
@@ -42,8 +42,8 @@ public class GenericSampleOrderServiceTest extends BaseWebContextSensitiveTest {
     @Before
     public void setUp() throws Exception {
         executeDataSetWithStateManagement("testdata/test-generic-sample-order.xml");
-        resyncSequence("clinlims.sample_seq", "clinlims.sample");
-        resyncSequence("clinlims.sample_item_seq", "clinlims.sample_item");
+        resyncSequence("sample_seq", "sample");
+        resyncSequence("sample_item_seq", "sample_item");
 
         AtomicInteger counter = new AtomicInteger(1);
         String twoDigitYear = String.valueOf(java.time.Year.now().getValue() % 100);
